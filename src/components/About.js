6c0591257b6ea96f4e5a70d8231e3b9d5ebf0a88
Masoud <masoud_mdar@yahoo.fr>
2021-06-30 17:01:28 +0200

    about page added

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,33 @@ const About = () => {
     return (
         <div>
             About Page
+            <p>
+            Anton Chekhov once said "Medicine is my lawful wife, and literature is my mistress". We know him of course as a great writer, not a great doctor!
+            </p>
+            <p>
+            Same as Chekhov’s passion about literature, coding programs for computers has always been my mistress. Since the 90s and “not so much fancy computers” era, I’ve always been fascinated by how someone can make computers do awesome things by saying the right words. Computer programming is the most realistic abracadabra that someone can find: “I will create as I speak” or I will create as I program!
+            </p>
+            <p>
+            My passion about computers and programming them started with saying Hello to the World in my oldest programming language friend, Basic. Sure, solving Project Euler problems with Basic is not as much fun as it seems, but programming’s inner charm brings us all the fun wee need! But this passion made a long way to become the first priority in my life.
+            </p>
+            <p>
+            As a passionate and curious child I continued my “binary journey”  by creating simple electronic kits and finally the life took me to the mechanical engineering world! I know, it seems to be a totally different world, but I assume that we can not blame a curious teenager seeking to know everything to satisfy their need for creating things. After inventing a machine (yeah, I’m an inventor, two points B) and engaging in some serious Iranian political movements, I found studying mechanical engineering too abstract to satisfy my need for creating. Of course studying under the shadow of a dictatorship also played it’s role by preventing the “rebellion” students to have their credits and long story short, I left my first bachelor without finishing it.
+            </p>
+            <p>
+            The passion and the need for creating things, with a little dressing of political consciousness took me towards the “not so much engineering” part of my life. This time, I found literature the proper tool to satisfy my need. I think you could be agree with me that literature and computer programming have something in common: using a syntax to create magic things that do not concretely exist! I studied the french literature, wrote some short stories and a novel and brought my logical algorithmic thinking habit to the magic world of literature. What about the computer programming? I spend nights with my “mistress” writing fun (?) programs in Basic language and telling everyone how fascinating is to write programs.
+            </p>
+            <p>
+            But even literature was not enough to satisfy my need and my passion pushed me towards the crossroad of my little knowledge: I came to France to study for a master’s degree in cultural studies. Well, when I say cultural studies I mean studying almost absolutely what you want! From video games and their role to help oppressed people to how the global political system works and from music and LGBT communities to how Internet changes the world! And guess what! Computer programming stayed my mistress. But this time, I tried the Python programming language to solve Project Euler problems and creating simple but awesome things from time to time. My old friend, Basic, was a little slow to properly satisfy my need.
+            </p>
+            <p>
+            After finishing my studies in cultural studies I finally found inner peace. Experiencing so many different fields, I was finally in a place where I knew what’s going on in the world and more important, what I really want. The only passion that has always been with me in my journey was my passion for computer programming. Now, it was time to spend some serious time with my life long mistress!
+            </p>
+            <p>
+            Of course, with my experience of living in a dictatorship, I wanted to make the world a better place. So, I started studying a part time PHD program in the Cultural Studies only to do my part in making the world a better place for everyone. But this time I let my joy of programming computers lead my life. One year ago, in the bizarre summer of 2020, I started learning programming seriously and with a strict schedule. Since then, I’ve coded almost every single day and have spent so many hours building projects and of course, logging things to the console trying to find out what the cookie is the problem of my code.
+            </p>
+            <p>
+            Computer Programming is above all my passion, my magic world and my preferred occupation. 
+            </p>
         </div>
     )
 }

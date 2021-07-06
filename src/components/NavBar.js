0e86ab08ee2cf1b01c1c3b2b9a3313303e740edc
Masoud <masoud_mdar@yahoo.fr>
2021-07-06 16:55:28 +0200

    navbar responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,10 +7,10 @@ const NavBar = (props) => {
         <div className="navbar">
             <div className="nav-logo"></div>
             <div className="nav-links">
-                <NavLink to="/portfolio" activeClassName="link-active" exact={true}>Home</NavLink>
-                <NavLink to="/projects" activeClassName="link-active">Projects</NavLink>
-                <NavLink to="/about" activeClassName="link-active">About</NavLink>
-                <NavLink to="/contact" activeClassName="link-active">Contact</NavLink>
+                <NavLink to="/portfolio" activeClassName="link-active" className="nav-link" exact={true}>Home</NavLink>
+                <NavLink to="/projects" activeClassName="link-active" className="nav-link">Projects</NavLink>
+                <NavLink to="/about" activeClassName="link-active" className="nav-link">About</NavLink>
+                <NavLink to="/contact" activeClassName="link-active" className="nav-link">Contact</NavLink>
             </div>
         </div>
     )

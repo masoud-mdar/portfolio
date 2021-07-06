a925320544aa934d910f39bacb93110869e184d3
Masoud <masoud_mdar@yahoo.fr>
2021-07-06 15:56:50 +0200

    tom component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,34 +3,14 @@ import {HashLink} from "react-router-hash-link"
 import {Link} from "react-router-dom"
 import ScrollButton from "./ScrollButton"
 import HomeProjects from "./HomeProjects"
+import Top from "./Top"
 
 const HomePage = () => {
 
     return (
         <div>
 
-            <div className="top-wrapper">
-                <div className="text">
-                    <div className="text-part">
-                        <p>Hi,</p>
-                        <p>My name is Masoud,</p>
-                        <p>I can talk to computers...</p>
-                        <p>Well, I'm a web developer!</p>
-                    </div>
-                    <div className="btn-wrapper">
-                        <div className="fake-borders">
-                            <HashLink smooth to="#projects" className="projects-button">My Projects</HashLink>
-                        </div>
-                        <div className="flash">
-                            <HashLink smooth to="#projects" className="flash-button"></HashLink>
-                        </div>
-                        
-                    </div>
-                        
-                    
-                </div>
-                <div className="top-part"></div>
-            </div>
+            <Top />
             
 
             <div className="container">
---
+++ src/components/Top.js
@@ -0,0 +1,32 @@
+import React from "react"
+import {HashLink} from "react-router-hash-link"
+
+const Top = () => {
+
+    return (
+        <div className="top-wrapper">
+            <div className="text">
+                <div className="text-part">
+                    <p>Hi,</p>
+                    <p>My name is Masoud,</p>
+                    <p>I can talk to computers...</p>
+                    <p>Well, I'm a web developer!</p>
+                </div>
+                <div className="btn-wrapper">
+                    <div className="fake-borders">
+                        <HashLink smooth to="#projects" className="projects-button">My Projects</HashLink>
+                    </div>
+                    <div className="flash">
+                        <HashLink smooth to="#projects" className="flash-button"></HashLink>
+                    </div>
+                    
+                </div>
+                    
+                
+            </div>
+            <div className="top-part"></div>
+        </div>
+    )
+}
+
+export default Top

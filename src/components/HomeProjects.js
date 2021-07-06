2f5d7f16d95bdfc50760b4341398a3f7f647227a
Masoud <masoud_mdar@yahoo.fr>
2021-07-06 15:53:15 +0200

    HomePage projects component

---
+++ src/components/HomeProjects.js
@@ -0,0 +1,152 @@
+import React from "react"
+
+const HomeProjects = (props) => {
+
+    return (
+        <div id="projects">
+
+            <div className="project">
+                <div className="project-details">
+                    <div className="project-title">
+                        <h2><a href="https://issue-tracker-masoud.netlify.app" target="blank" className="project-link">Issue Tracker</a></h2>
+                    </div>
+                    <div className="project-skills">
+                        <p>HTML</p>
+                        <p>CSS</p>
+                        <p>Javascript</p>
+                        <p>React</p>
+                        <p>Node.js</p>
+                        <p>Passport.js</p>
+                        <p>ES6</p>
+                        <p>Express.js</p>
+                        <p>MongoDB</p>
+                    </div>
+                    
+                </div>
+
+                <div className="project-code">
+                    <a href="https://issue-tracker-masoud.netlify.app" target="blank">Live Website</a>
+                    <a href="https://github.com/masoud-mdar/Issue-Tracker" target="blank">Code</a>
+                </div>
+
+            </div>
+
+            <div className="project">
+                <div className="project-details">
+                    <div className="project-title">
+                        <h2><a href="https://personal-library-masoud.netlify.app" target="blank" className="project-link">Personal Library</a></h2>
+                    </div>
+                    <div className="project-skills">
+                        <p>HTML</p>
+                        <p>CSS</p>
+                        <p>Javascript</p>
+                        <p>React</p>
+                        <p>Node.js</p>
+                        <p>ES6</p>
+                        <p>Express.js</p>
+                        <p>MongoDB</p>
+                    </div>
+                    
+                </div>
+                <div className="project-code">
+                    <a href="https://personal-library-masoud.netlify.app" target="blank">Live Website</a>
+                    <a href="https://github.com/masoud-mdar/Personal-Library" target="blank">Code</a>
+                </div>
+            </div>
+
+            <div className="project">
+                <div className="project-details">
+                    <div className="project-title">
+                        <h2><a href="https://sudoku-solver-masoud.netlify.app" target="blank" className="project-link">Sudoku Solver</a></h2>
+                    </div>
+                    <div className="project-skills">
+                        <p>HTML</p>
+                        <p>CSS</p>
+                        <p>Javascript</p>
+                        <p>React</p>
+                        <p>Node.js</p>
+                        <p>ES6</p>
+                        <p>Express.js</p>
+                    </div>
+                    
+                </div>
+                <div className="project-code">
+                    <a href="https://sudoku-solver-masoud.netlify.app" target="blank">Live Website</a>
+                    <a href="https://github.com/masoud-mdar/Sudoku-Solver" target="blank">Code</a>
+                </div>
+            </div>
+
+            <div className="project">
+                <div className="project-details">
+                    <div className="project-title">
+                        <h2><a href="https://exercise-tracker-msd.netlify.app" target="blank" className="project-link">Exercise Tracker</a></h2>
+                    </div>
+                    <div className="project-skills">
+                        <p>HTML</p>
+                        <p>CSS</p>
+                        <p>Javascript</p>
+                        <p>React</p>
+                        <p>Node.js</p>
+                        <p>ES6</p>
+                        <p>Express.js</p>
+                        <p>MongoDB</p>
+                        <p>Mongoose</p>
+                    </div>
+                    
+                </div>
+                <div className="project-code">
+                    <a href="https://exercise-tracker-msd.netlify.app" target="blank">Live Website</a>
+                    <a href="https://github.com/masoud-mdar/Exercise-Tracker" target="blank">Code</a>
+                </div>
+            </div>
+
+            <div className="project">
+                <div className="project-details">
+                    <div className="project-title">
+                        <h2><a href="https://masoud-mdar.github.io/Meme-Generator" target="blank" className="project-link">Meme Generator</a></h2>
+                    </div>
+                    <div className="project-skills">
+                        <p>HTML</p>
+                        <p>CSS</p>
+                        <p>Javascript</p>
+                        <p>React</p>
+                        <p>ES6</p>
+                        <p>API</p>
+
+                    </div>
+                    
+                </div>
+                <div className="project-code">
+                    <a href="https://masoud-mdar.github.io/Meme-Generator" target="blank">Live Website</a>
+                    <a href="https://github.com/masoud-mdar/Meme-Generator" target="blank">Code</a>
+                </div>
+            </div>
+
+            <div className="project">
+                <div className="project-details">
+                    <div className="project-title">
+                        <h2><a href="https://metric-imperial-converter.netlify.app" target="blank" className="project-link">Unit Converter</a></h2>
+                    </div>
+                    <div className="project-skills">
+                        <p>HTML</p>
+                        <p>CSS</p>
+                        <p>Javascript</p>
+                        <p>React</p>
+                        <p>Node.js</p>
+                        <p>ES6</p>
+                        <p>Express.js</p>
+                    </div>
+                    
+                </div>
+                <div className="project-code">
+                    <a href="https://metric-imperial-converter.netlify.app" target="blank">Live Website</a>
+                    <a href="https://github.com/masoud-mdar/Metric-Imperial-Converter" target="blank">Code</a>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default HomeProjects

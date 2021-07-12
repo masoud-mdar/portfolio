4cb3d1f0aa20311e381a140f804c5a0247a6ad17
Masoud <masoud_mdar@yahoo.fr>
2021-07-12 21:18:55 +0200

    styles cleaning

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ import AppRouter from "./components/AppRouter"
 import "./styles.css"
 import "./styles/navbar.css"
 import "./styles/top.css"
+import "./styles/mainAndProjects.css"
 
 ReactDOM.render(<AppRouter />, document.getElementById("root"))
